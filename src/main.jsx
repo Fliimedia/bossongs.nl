b8c18abbd81b54bdcf31585386dc17b422cde830
Fliimedia <info@flii.nl>
2026-08-02 10:30:54 +0200

    Mount BruiloftSiteV2

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BruiloftSiteV2 from "./BruiloftSiteV2.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BruiloftSiteV2 />
+  </React.StrictMode>
+);
